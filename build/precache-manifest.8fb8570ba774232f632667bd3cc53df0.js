41c03b2e030fe68ac870b881dcf252266f953f76
ShafeequeAli <shafeeeeque@gmail.com>
2020-09-23 09:43:13 +0530

    gh-pages update

---
+++ build/precache-manifest.8fb8570ba774232f632667bd3cc53df0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ffcf6ab77748a2c7a7ee6d56cdd914",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "e17ba698174a18d414b9",
+    "url": "/movie/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "39dac5eaec08e1ac280c",
+    "url": "/movie/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "e17ba698174a18d414b9",
+    "url": "/movie/static/js/2.ac1fc42a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie/static/js/2.ac1fc42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39dac5eaec08e1ac280c",
+    "url": "/movie/static/js/main.4afaf5ef.chunk.js"
+  },
+  {
+    "revision": "e41199f4a9a299bf07fe",
+    "url": "/movie/static/js/runtime-main.74acc5d5.js"
+  },
+  {
+    "revision": "4d483a7cf44753c15913570e6d95cbdb",
+    "url": "/movie/static/media/wave.4d483a7c.gif"
+  }
+]);
